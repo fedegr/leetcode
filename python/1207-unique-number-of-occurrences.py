ce0a05f896ed16e173d718d267ff85780e66e80d
Federico Grosso <fgrosso85@gmail.com>
2025-02-01 18:08:36 +0100

    1207-unique-number-of-occurrences

---
+++ python/1207-unique-number-of-occurrences.py
@@ -0,0 +1,10 @@
+# 1207-unique-number-of-occurrences
+from typing import List
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        occurrences = {}
+        for n in arr:
+            occurrences[n] = occurrences.get(n, 0) + 1
+        
+        return len(occurrences) == len(set(occurrences.values()))
