bbf4b7627326bf5980f172e02fea05a47d8d4d9e
Federico Grosso <fgrosso85@gmail.com>
2025-02-12 17:22:39 +0100

    leetcode daily - 2025-02-12 - Max Sum of a Pair With Equal Sum of Digits

--- python/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ python/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,4 +1,5 @@
 from typing import List
+import heapq
 
 
 class Solution:
@@ -6,15 +7,19 @@ class Solution:
         digit_sum = {}
         for n in nums:
             ds = sum(int(c) for c in str(n))
-            digit_sum.setdefault(ds, list()).append(n)
+            numbers_with_ds = digit_sum.setdefault(ds, list())
+            if len(numbers_with_ds) <= 1:
+                heapq.heappush(numbers_with_ds, n)
+            else:
+                heapq.heappushpop(numbers_with_ds, n)
+
         
         best = -1
         for ds, numbers in digit_sum.items():
             if len(numbers) <= 1:
                 continue
-            s = sum(sorted(numbers, reverse=True)[:2])
+            s = numbers[0] + numbers[1]
             if best < s:
                 best = s
 
         return best
-    
