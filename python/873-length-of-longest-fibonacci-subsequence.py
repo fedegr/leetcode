5f95e8af6346a68be0c71d5efa95de0966152e70
Federico Grosso <fgrosso85@gmail.com>
2025-02-27 22:45:15 +0100

    leetcode daily - 2025-02-27 - Length of Longest Fibonacci Subsequence - add DP

--- python/873-length-of-longest-fibonacci-subsequence.py
+++ python/873-length-of-longest-fibonacci-subsequence.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def lenLongestFibSubseqBruteforce(arr: List[int]) -> int:
     values = set(arr)
     best = 0
@@ -19,7 +22,30 @@ def lenLongestFibSubseqBruteforce(arr: List[int]) -> int:
     return best
 
 
+def lenLongestFibSubseqDP(arr: List[int]) -> int:
+    idx = { n: i for i, n in enumerate(arr) }
+    best = 0
+    longest = [[0 for _ in range(len(arr))] for _ in range(len(arr))]
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            b = arr[i]
+            c = arr[j]
+            a = c - b
+            prev = idx.get(a, None)
+            if prev is not None and a < b:
+                longest[i][j] = longest[prev][i] + 1
+                best = max(best, longest[i][j])
+            else:
+                longest[i][j] = 2
+
+    if best < 3:
+        best = 0
+    
+    return best
+
+
+
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
-        return lenLongestFibSubseqBruteforce(arr)
+        return lenLongestFibSubseqDP(arr)
                 
