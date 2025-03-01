5b56f31407204ffea251ade3b71927be79e65443
Federico Grosso <fgrosso85@gmail.com>
2025-03-01 21:28:49 +0100

    leetcode75 - #399

---
+++ python/399-evaluate-division.py
@@ -0,0 +1,38 @@
+from typing import List
+from collections import deque
+
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        relation = {}
+        for (a, b), d in zip(equations, values):
+            relation.setdefault(a, {}).setdefault(b, d)
+            relation.setdefault(b, {}).setdefault(a, 1.0 / d)
+        # for a in relation.keys():
+        #     relation[a][a] = 1.0
+        
+        results = []
+        for a, b in queries:
+            if a not in relation or b not in relation:
+                results.append(-1.0)
+                continue
+            
+            answer = -1.0
+            visited = set()
+            stack = deque()
+            stack.append((a, 1.0))
+            while stack:
+                current, value = stack.popleft()
+                if current not in relation[a]:
+                    relation[a][current] = value
+                if current in visited:
+                    continue
+                visited.add(current)
+                if current == b:
+                    answer = value
+                    break
+                for n, v in relation[current].items():
+                    stack.append((n, value * v))
+            results.append(answer)
+
+        return results
