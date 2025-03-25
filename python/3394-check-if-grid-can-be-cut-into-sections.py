a140d1147a109cbefbdd21b3d56a4e1b2a4ca764
Federico Grosso <fgrosso85@gmail.com>
2025-03-25 23:49:23 +0100

    leetcode daily - 2025-03-25 - Check if Grid can be Cut into Sections

---
+++ python/3394-check-if-grid-can-be-cut-into-sections.py
@@ -0,0 +1,45 @@
+from typing import List
+
+
+def lines_without_overlap(rectangles: List[List[int]], y: bool=False) -> bool:
+    # component = 1 if y else 0
+    # rectangles.sort(key=lambda x: x[component])
+
+    # cuts = 0
+    # candidates = set(rect[2+component] for rect in rectangles)
+    # candidates.discard(max(candidates))
+
+    # all_cuts = list()
+    # for c in candidates:
+    #     if any(rect[component] < c < rect[2+component] for rect in rectangles):
+    #         continue
+
+    #     all_cuts.append(c)
+    #     cuts += 1
+    #     if cuts == 2:
+    #         return True
+    # return False
+
+
+    component = 1 if y else 0
+    rectangles.sort(key=lambda x: (x[component], x[component+2]))
+
+    cuts = 0
+    last_end = rectangles[0][component+2]
+    for i in range(1, len(rectangles)):
+        rect = rectangles[i]
+        start = rect[component]
+        end = rect[component + 2]
+        if start >= last_end:
+            last_end = end
+            cuts += 1
+            if cuts == 2:
+                return True
+        else:
+            last_end = max(last_end, end)
+
+    return False
+
+class Solution:
+    def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
+        return lines_without_overlap(rectangles) or lines_without_overlap(rectangles, y=True)
