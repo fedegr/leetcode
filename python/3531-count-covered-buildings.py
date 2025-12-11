94ba4288e38ba5e1fd8b392746e21fe680bb5bd3
Federico Grosso <fgrosso85@gmail.com>
2025-12-11 09:29:08 +0100

    leetcode daily - 2025-12-11 - Count Covered Buildings

---
+++ python/3531-count-covered-buildings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
+        xs = {}
+        ys = {}
+
+        for (x, y) in buildings:
+            min_y, max_y = ys.get(x, (y, y))
+            ys[x] = (min(min_y, y), max(max_y, y))
+
+            min_x, max_x = xs.get(y, (x, x))
+            xs[y] = (min(min_x, x), max(max_x, x))
+
+        
+        return sum(
+            1
+            for x, y in buildings
+            if (
+                xs[y][0] < x and x < xs[y][1] and
+                ys[x][0] < y and y < ys[x][1]
+            )
+        )
